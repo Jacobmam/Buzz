7c4a419aa6e2ecd7e7433de87c7fdb19cef7891f
Jacobmam <jacobmam1335@gmail.com>
2025-02-10 17:00:09 +0100

    Create Project

---
+++ Buzz/Buzz/BuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuzzApp.swift
+//  Buzz
+//
+//  Created by Jacob Mampuya on 10.02.25.
+//
+
+import SwiftUI
+
+@main
+struct BuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
